8ca17d771f149f16d5e47c995219695320dae05c
harshmane1892 <135492448+harshmane1892@users.noreply.github.com>
2023-06-04 21:05:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ OWNER_ID = 1351777526 #write owner's user id here.. get it from @MissRose_Bot by
 CHANNELS = ["@surpriseloots"] #add channels to be checked here in the format - ["Channel 1", "Channel 2"] 
               #you can add as many channels here and also add the '@' sign before channel username
 Daily_bonus = 0.01 #Put daily bonus amount here!
-Mini_Withdraw = 1  #remove 0 and add the minimum withdraw u want to set
+Mini_Withdraw = 0.01 #remove 0 and add the minimum withdraw u want to set
 Per_Refer = 0.1 #add per refer bonus here
 
 bot = telebot.TeleBot(BOT_TOKEN)
